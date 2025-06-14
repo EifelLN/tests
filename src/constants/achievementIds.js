1b00b0b1f8ab01d7f446728b012bddb72406fda3
Eifel.Navara <120072946+EifelLN@users.noreply.github.com>
2025-06-14 18:37:26 +0700

    Use structured achievement IDs

---
+++ src/constants/achievementIds.js
@@ -0,0 +1,12 @@
+export const ACHIEVEMENT_IDS = {
+  PROFILE_COMPLETE: 'ach001',
+  FIRST_LESSON: 'ach002',
+  FIRST_COURSE: 'ach003',
+  COURSE_MASTER: 'ach004',
+  COURSE_LEGEND: 'ach005',
+  STREAK_3: 'ach006',
+  STREAK_7: 'ach007',
+  STREAK_30: 'ach008',
+  LEVEL_5: 'ach009',
+  LEVEL_10: 'ach010'
+};
